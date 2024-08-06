4a914e4947857d039eac002b132fd6b7cc2f6f15
BlitzKriegCod <yuleskygonzalez534@gmail.com>
2024-08-06 04:04:20 -0400

    Adding JWT auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,26 @@
 const { response } = require("express")
+const User = require('../models/user.model')
 
-module.exports.login = (req, res = response) =>{
+
+module.exports.login = async(req, res = response) =>{
     
+
+try {
+    
+    const {email} = req.body
+    const user = await User.findOne({email})
+    if(!user){
+       return res.status(400).json(`Email or password is incorrect`)
+    }
+
+
+} catch (error) {
+    res.status(500).json(`Opps ${error} ,try contacting the admin, the backend is burning!!!`)
+}
+
+
+
+
+
     res.json({msg:'this route works'})
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,12 @@
-const {check,Router} = require('express')
+const {Router} = require('express')
+const {check} = require('express-validator')
 const {login} = require('../controllers/auth.controller')
+const { validate } = require('../models/user.model')
+const { validateUserFields, validateFields } = require('../middlewares/validators')
 
 
 const router = Router()
-router.post('/login',login)
+router.post('/login',[check('email','You need send an email address example@gmail.com').isEmail(),
+    check('password','Something is wrong with the password, check this again :)').not().isEmpty(),validateFields],login);
 
 module.exports = router
