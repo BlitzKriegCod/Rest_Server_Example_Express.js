9e872a38e095ecac9aab483ef44efd909f949f36
BlitzKriegCod <yuleskygonzalez534@gmail.com>
2024-08-06 15:16:09 -0400

    Add Role Validation

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -1,7 +1,7 @@
 const JWT = require('jsonwebtoken')
+const User = require('../models/user.model')
 
-
-module.exports.ValidateJWT = (req,res,next)=>{
+module.exports.ValidateJWT = async(req,res,next)=>{
 
     let tk = req.header('Authorization')
     /**/
@@ -14,8 +14,21 @@ module.exports.ValidateJWT = (req,res,next)=>{
 
     try {
         if(JWT.verify(tk,process.env.SECRETKEY)){
+        const {uid} = JWT.verify(tk,process.env.SECRETKEY)
+        req.uid = uid
+
+        const user = await User.findById(uid)
+        if(!user){
+            return res.status(401).json({
+                msg:"User not exist"
+            })
+        }
+            req.user = user
         console.log(tk)
-        next()}
+       
+       next()
+    
+    }
             
     } catch (error) {
     res.status(401).json({
